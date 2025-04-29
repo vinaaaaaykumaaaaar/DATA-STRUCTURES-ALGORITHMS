e91e0319c593756f4d143cb203f29486c4f4db61
Vinay Kumar <vinayp.21.beis@acharya.ac.in>
2025-04-29 17:36:08 +0000

    solved union problem

--- ARRAYS/union.cpp
+++ ARRAYS/union.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<set>
+#include<vector>
 
 using namespace std;
 
@@ -23,8 +24,58 @@ void naive_solution()
 
 }
 
+void optimal_solution()
+{
+
+	// time complexity -> O(n + m) -> n and m are sizes of the arrays
+	// space complexity -> O(n+m) -> n and m are sizes of the arrays
+	
+	int i=0, j=0;
+	vector<int> uni;
+
+	while(i<5 && j<5)
+	{
+		if(a[i] <= b[j])
+		{
+			if(uni.size() ==0 || uni.back() != a[i])
+				uni.push_back(a[i]);
+			i++;
+		}
+		else
+		{
+			if(uni.size() ==0 || uni.back() != b[j])
+				uni.push_back(b[j]);
+			j++;
+		}
+	}
+
+	while(i<5)
+	{
+		if(uni.size() == 0 || uni.back() != a[i])
+			uni.push_back(a[i]);
+		i++;
+	}
+
+	while(j<5)
+	{
+		if(uni.size() == 0 || uni.back() != b[j])
+			uni.push_back(b[j]);
+		j++;
+	}
+
+	for(auto x:uni)
+		cout<<x<<" ";
+	cout<<endl;
+
+}
+
+	
+
+	
+
 int main()
 {
-	naive_solution();
+	// naive_solution();
+	optimal_solution();
 	return 0;
 }
