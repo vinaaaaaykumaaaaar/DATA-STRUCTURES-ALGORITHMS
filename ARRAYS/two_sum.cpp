aec0c6883db9e4d9e5c1a19e973a46b9138c3ede
Vinay Kumar <vinayp.21.beis@acharya.ac.in>
2025-04-30 17:29:23 +0000

    solved two pointers

--- ARRAYS/two_sum.cpp
+++ ARRAYS/two_sum.cpp
@@ -1,110 +1,56 @@
 #pragma GCC optimize("03")
 #pragma GCC target ("sse4")
 
-#include <bits/stdc++.h>
+#include <iostream>
+#include <map>
+#include <vector>
+#include <set>
+#include <algorithm>
 
 using namespace std;
 
 typedef long long ll;
 typedef long double ld;
-typedef complex<ld> cd;
+
+ll a[10] = {1,2,3,4,5,6,7,8,9,10};
  
-typedef pair<int, int> pi;
-typedef pair<ll,ll> pl;
-typedef pair<ld,ld> pd;
-  
-typedef vector<int> vi;
-typedef vector<ld> vd;
-typedef vector<ll> vl;
-typedef vector<pi> vpi;
-typedef vector<pl> vpl;
-typedef vector<cd> vcd;\
-
-template<class T> using pq = priority_queue<T>;
-template<class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
-
-#define FOR(i, a, b) for (int i=a; i<(b); i++)
-#define F0R(i, a) for (int i=0; i<(a); i++)
-#define FORd(i,a,b) for (int i = (b)-1; i >= a; i--)
-#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
-#define trav(a,x) for (auto& a : x)
-#define uid(a, b) uniform_int_distribution<int>(a, b)(rng)
-
-#define sz(x) (int)(x).size()
-#define mp make_pair
-#define pb push_back
-#define f first
-#define s second
-#define lb lower_bound
-#define ub upper_bound
-#define all(x) x.begin(), x.end()
-#define ins insert
-
-template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
-template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
-
-mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
-
-const int MOD = 1000000007;
-const char nl = '\n';
-const int MX = 100001; 
-
-int a[10] = {1,2,3,4,5,6,7,8,9,10};
-int key = 6;
-
 void naive_solution()
 {
-    // time complexity -> O(n^2) 
-    // space complexity -> O(1)
-
-    for(int i=0; i<10; i++)
-        for(int j=i+1; j<10;j++)
-            if(key == a[i] + a[j])
-                cout<<"Two Pointers are: "<<a[i]<<" "<<a[j]<<endl;
+    // time complexity -> 
+    // space complexity ->
       
 }
 
 void better_solution()
 {
-    // time complexity -> O(nlogn)
-    // space complexity -> O(n)
+    // time complexity ->
+    // space complexity ->
 
-    map<int ,int> m ={};
-    for(int i=0;i<10;i++)
-        m[a[i]] = 1;
-
-    for(int i=0; i<10; i++)
-    {
-        int diff = key - a[i];
-        if(m[diff])
-            cout<<"Two Pointers are: "<<a[i] <<" "<<diff<<endl;
-    }
+    
 
 }
 
 void optimal_solution()
 {
-    // time complexity -> O(n) 
-    // space complexity -> O(1)
+    // time complexity ->
+    // space complexity ->
+
     int i=0 , j=9;
+    int k = 6;
     while(i<j)
     {
-    if (a[i] + a[j] == key )
-    {
-        cout<<a[i]<<" "<<a[j]<<endl;
-    }
-    else if(key > a[i] + a[j])
-    {
-        j--;
+        int sum = a[i]+a[j];
+        if(sum == k)
+        {
+            cout<<a[i]<<" "<<a[j]<<endl;
+            break;
+        }
 
-    }
-    else
-    {
-        i++;
+        else if(sum > k) j--;
+        else i++;
     }
 
-        
-    }
+    
 
 }
 
@@ -113,7 +59,7 @@ int main()
     cin.tie(0)->sync_with_stdio(0); 
     cin.exceptions(cin.failbit);
             
-//    naive_solution();
+   // naive_solution();
     // better_solution();
      optimal_solution();
                                       
