c1c8dcaaa1ac77d574e8c9d4f19a432be1f03908
Vinay Kumar <vinayp.21.beis@acharya.ac.in>
2025-07-09 11:16:02 +0530

    solved

--- 13-GRAPHS/depthFIrstSearch.cpp
+++ 13-GRAPHS/depthFIrstSearch.cpp
@@ -1,27 +1,8 @@
-#include "adjList.hpp"
-#include "adjMatrix.hpp"
+#include <iostream>
+using namespace std;
 
 int main()
 {
-    AdjacencyListGraph g(5);
-    g.addEdge(0, 1);
-    g.addEdge(0, 4);
-    g.addEdge(1, 2);
-    g.addEdge(1, 3);
-    g.addEdge(1, 4);
-    g.addEdge(2, 3);
-    g.addEdge(3, 4);
-    g.display();
-
-    std::cout << "--------------------------" << std::endl;
-    AdjacencyMatrixGraph v(5);
-    v.addEdge(0, 1);
-    v.addEdge(0, 4);
-    v.addEdge(1, 2);
-    v.addEdge(1, 3);
-    v.addEdge(1, 4);
-    v.addEdge(2, 3);
-    v.addEdge(3, 4);
-    v.display();
+    std::cout << "Vinay is good boy " << std::endl;
     return 0;
 }
