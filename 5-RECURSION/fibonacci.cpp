16952a9023153a515f07b21b64ed9a72e8d789cd
Vinay Kumar <vinayp.21.beis@acharya.ac.in>
2025-07-16 17:40:33 +0530

    solved

--- 5-RECURSION/fibonacci.cpp
+++ 5-RECURSION/fibonacci.cpp
@@ -2,11 +2,12 @@
 
 int fibonacci(int number)
 {
-    if (number <= 1)
+    if (number == 0)
+        return 0;
+    if (number == 1)
         return 1;
 
-    return number * fibonacci(number - 1);
-    return 0;
+    return fibonacci(number - 1) + fibonacci(number - 2);
 }
 
 int main()
