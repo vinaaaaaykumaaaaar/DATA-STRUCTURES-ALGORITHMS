ae35e0dedef051889f58ce7d57393bc77475b879
Vinay Kumar <vinayp.21.beis@acharya.ac.in>
2025-07-17 14:00:18 +0530

    solved

---
+++ template.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <map>
+#include <stack>
+#include <queue>
+#include <list>
+
+using namespace std;
+
+void solve()
+{
+}
+
+int main()
+{
+
+    vector<int> a = {};
+    int n, m, i, k;
+
+    solve();
+    return 0;
+}
