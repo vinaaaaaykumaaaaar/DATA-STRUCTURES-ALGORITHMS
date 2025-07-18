a72e129b98fc6f5c277b5806f86e68d9b230454b
Vinay Kumar <vinayp.21.beis@acharya.ac.in>
2025-07-18 11:21:20 +0530

    solved

---
+++ 2-BINARY-SEARCH/upper_bound.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <map>
+#include <stack>
+#include <queue>
+#include <list>
+
+using namespace std;
+
+int solve(vector<int> &a, int s, int e, int t)
+{
+    int ans = e; // Initialize ans (or could be a.size() if searching entire array)
+
+    while (s <= e)
+    {
+        int m = s + (e - s) / 2; // Avoid potential overflow
+
+        if (a[m] > t)
+        {
+            ans = m;
+            e = m - 1; // Search left half
+        }
+        else
+        {
+            s = m + 1; // Search right half
+        }
+    }
+
+    return ans;
+}
+
+int main()
+{
+    vector<int> a = {3, 5, 8, 15, 19};
+
+    cout << solve(a, 0, a.size() - 1, 8) << endl; // Pass a.size()-1, not a.size()
+    return 0;
+}
