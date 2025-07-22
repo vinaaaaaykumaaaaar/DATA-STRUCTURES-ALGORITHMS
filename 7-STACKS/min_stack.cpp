b5f9ae51d811639494a3142052c7bccf1724de6f
Vinay Kumar <vinayp.21.beis@acharya.ac.in>
2025-07-22 22:23:38 +0530

    solved

---
+++ 7-STACKS/min_stack.cpp
@@ -0,0 +1,13 @@
+// Implementation of minimum stack
+
+#include <iostream>
+#include <stack>
+#include <map>
+// #include<pair>
+
+int main()
+{
+    int_fast64_t mini = INT_FAST16_MAX;
+
+    std::cout << mini << std::endl;
+}
