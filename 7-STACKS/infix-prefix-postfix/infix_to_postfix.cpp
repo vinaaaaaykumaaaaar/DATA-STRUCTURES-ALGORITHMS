e19f4a8c742f08956c7c9b22bd9d605da9514c39
Vinay Kumar <vinayp.21.beis@acharya.ac.in>
2025-07-22 22:04:07 +0530

    solved

--- 7-STACKS/infix-prefix-postfix/infix_to_postfix.cpp
+++ 7-STACKS/infix-prefix-postfix/infix_to_postfix.cpp
@@ -6,25 +6,24 @@
 
 std::string infix_to_postfix(std::string infix_expression)
 {
+    int_fast64_t index = 0;
     std::string postfix_expression = "";
     std::stack<char> expression_stack;
-    int_fast64_t index = 0;
-    int_fast64_t size = infix_expression.length();
 
-    std::map<char, int> priority_table;
-    priority_table['^'] = 3;
-    priority_table['*'] = 2;
-    priority_table['/'] = 2;
-    priority_table['-'] = 1;
-    priority_table['+'] = 1;
+    std::map<char, int_fast64_t> priority_value;
+    priority_value['^'] = 3;
+    priority_value['/'] = 2;
+    priority_value['*'] = 2;
+    priority_value['+'] = 1;
+    priority_value['-'] = 1;
 
-    while (index < size)
+    while (index < infix_expression.length())
     {
         char character = infix_expression[index];
 
         if (character >= 'A' && character <= 'Z' || character >= 'a' && character <= 'z' || character >= '0' && character <= '9')
         {
-            postfix_expression = postfix_expression + character;
+            postfix_expression += character;
         }
         else if (character == '(')
         {
@@ -32,18 +31,19 @@ std::string infix_to_postfix(std::string infix_expression)
         }
         else if (character == ')')
         {
+
             while (!expression_stack.empty() && expression_stack.top() != '(')
             {
-                postfix_expression = postfix_expression + expression_stack.top();
+                postfix_expression += expression_stack.top();
                 expression_stack.pop();
             }
             expression_stack.pop();
         }
         else
         {
-            while (!expression_stack.empty() && expression_stack.top() != '(' && priority_table[character] <= priority_table[expression_stack.top()])
+            while (!expression_stack.empty() && expression_stack.top() != '(' && priority_value[character] <= priority_value[expression_stack.top()])
             {
-                postfix_expression = postfix_expression + expression_stack.top();
+                postfix_expression += expression_stack.top();
                 expression_stack.pop();
             }
             expression_stack.push(character);
@@ -54,7 +54,7 @@ std::string infix_to_postfix(std::string infix_expression)
 
     while (!expression_stack.empty())
     {
-        postfix_expression = postfix_expression + expression_stack.top();
+        postfix_expression += expression_stack.top();
         expression_stack.pop();
     }
 
