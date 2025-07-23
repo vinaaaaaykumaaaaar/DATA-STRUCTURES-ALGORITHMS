cb5542f450b7b80efa5e67b7ccec936c443b44fd
Vinay Kumar <vinayp.21.beis@acharya.ac.in>
2025-07-23 11:40:26 +0530

    solved

---
+++ 1-ARRAYS/basic-problems/generate_subarrays.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <map>
+#include <stack>
+#include <queue>
+#include <list>
+#include <unordered_map>
+
+using namespace std;
+
+void solve(vector<int> &a)
+{
+
+    for (int i = 0; i < a.size(); ++i)
+    {
+        for (int j = i; j < a.size(); ++j)
+        {
+            for (int k = i; k <= j; ++k)
+            {
+                cout << a[k] << " ";
+            }
+            cout << endl;
+        }
+    }
+}
+
+int main()
+{
+
+    vector<int> a = {1, 2, 3};
+    vector<int> b = {1, 2};
+    vector<int> c = {1, 2, 3, 4};
+
+    cout << "-----------------------------------" << endl;
+    solve(a);
+    cout << "-----------------------------------" << endl;
+    solve(b);
+    cout << "-----------------------------------" << endl;
+    solve(c);
+    cout << "-----------------------------------" << endl;
+    return 0;
+}
