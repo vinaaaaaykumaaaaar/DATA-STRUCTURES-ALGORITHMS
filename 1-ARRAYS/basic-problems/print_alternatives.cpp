0e3e2e7d6125997693ff7277cc8930781f89db3b
Vinay Kumar <vinayp.21.beis@acharya.ac.in>
2025-07-23 10:35:18 +0530

    solved

--- 1-ARRAYS/basic-problems/print_alternatives.cpp
+++ 1-ARRAYS/basic-problems/print_alternatives.cpp
@@ -12,12 +12,15 @@ using namespace std;
 
 void solve(vector<int> &a)
 {
+    for (int i = 0; i < a.size(); i += 2)
+        cout << a[i] << " ";
+    cout << endl;
 }
 
 int main()
 {
 
-    vector<int> a = {};
+    vector<int> a = {-5, 1, 4, 2, 12};
 
     solve(a);
     return 0;
