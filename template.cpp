72b70d1436a3deab908be37de2dcf16d1aca9cd5
Vinay Kumar <vinayp.21.beis@acharya.ac.in>
2025-07-24 12:57:33 +0530

    solved

--- template.cpp
+++ template.cpp
@@ -1,16 +1,16 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
-#include <map>
-#include <stack>
-#include <queue>
-#include <list>
-#include <unordered_map>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-void solve(vector<int> &a)
+void brute_force(vector<int> &a)
+{
+}
+
+void better(vector<int> &a)
+{
+}
+
+void optimal(vector<int> &a)
 {
 }
 
@@ -18,7 +18,35 @@ int main()
 {
 
     vector<int> a = {};
+    vector<int> b = {};
+    vector<int> c = {};
 
-    solve(a);
+    vector<vector<int>> test_cases = {a, b, c};
+
+    cout << "-----------------------------------------------------------------" << endl;
+
+    // Brute force approach test case
+    cout << "--------------------- B R U T E - F O R C E ---------------------" << endl;
+    for (auto test_case : test_cases)
+    {
+        brute_force(test_case);
+    }
+
+    // Better approach test case
+    cout << "-------------------------- B E T T E R --------------------------" << endl;
+    for (auto test_case : test_cases)
+    {
+        better(test_case);
+    }
+
+    // Optimal approach test case
+    cout << "------------------------  O P T I M A L  ------------------------" << endl;
+    for (auto test_case : test_cases)
+    {
+        better(test_case);
+    }
+
+    cout << "-----------------------------------------------------------------" << endl;
+
     return 0;
 }
