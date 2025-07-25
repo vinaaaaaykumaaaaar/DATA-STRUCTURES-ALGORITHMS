e6007756ddc85bb5d61b8f975d92870e6dfba496
Vinay Kumar <vinayp.21.beis@acharya.ac.in>
2025-07-25 20:06:55 +0530

    solved

--- 1-ARRAYS/easy/longestSubarray_ksum.cpp
+++ 1-ARRAYS/easy/longestSubarray_ksum.cpp
@@ -52,7 +52,10 @@ void brute_force(vector<int> a, int k)
 
 void better(vector<int> a, int k)
 {
-    map<int, int> m;
+
+    // tc ->  O(n);
+    // sp -> O(n);
+    unordered_map<int, int> m;
 
     int mx = 0, sum = 0;
 
@@ -76,8 +79,37 @@ void better(vector<int> a, int k)
     cout << mx << endl;
 }
 
-void optimal(vector<int> &a)
+void optimal(vector<int> a, int k)
 {
+
+    // tc-> O(n);
+    // sc -> O(1);
+
+    int n = a.size(); // size of the array.
+
+    int left = 0, right = 0; // 2 pointers
+    long long sum = a[0];
+    int maxLen = 0;
+
+    while (right < n)
+    {
+        while (left <= right && sum > k)
+        {
+            sum -= a[left++];
+        }
+
+        if (sum == k)
+        {
+            maxLen = max(maxLen, right - left + 1);
+        }
+
+        right++;
+
+        if (right < a.size())
+            sum += a[right];
+    }
+
+    cout << maxLen << endl;
 }
 
 int main()
@@ -102,65 +134,9 @@ int main()
 
     // Optimal approach test case
     cout << "------------------------  O P T I M A L  ------------------------" << endl;
-    for (auto test_case : test_cases)
-    {
-        optimal(test_case);
-    }
-
+    optimal(a, 5);
+    optimal(b, 10);
     cout << "-----------------------------------------------------------------" << endl;
 
     return 0;
 }
-
-/*
-
-
-
-#include <bits/stdc++.h>
-using namespace std;
-
-int getLongestSubarray(vector<int>& a, long long k) {
-    int n = a.size(); // size of the array.
-
-    map<long long, int> preSumMap;
-    long long sum = 0;
-    int maxLen = 0;
-    for (int i = 0; i < n; i++) {
-        //calculate the prefix sum till index i:
-        sum += a[i];
-
-        // if the sum = k, update the maxLen:
-        if (sum == k) {
-            maxLen = max(maxLen, i + 1);
-        }
-
-        // calculate the sum of remaining part i.e. x-k:
-        long long rem = sum - k;
-
-        //Calculate the length and update maxLen:
-        if (preSumMap.find(rem) != preSumMap.end()) {
-            int len = i - preSumMap[rem];
-            maxLen = max(maxLen, len);
-        }
-
-        //Finally, update the map checking the conditions:
-        if (preSumMap.find(sum) == preSumMap.end()) {
-            preSumMap[sum] = i;
-        }
-    }
-
-    return maxLen;
-}
-
-int main()
-{
-    vector<int> a = {2, 3, 5, 1, 9};
-    long long k = 10;
-    int len = getLongestSubarray(a, k);
-    cout << "The length of the longest subarray is: " << len << "\n";
-    return 0;
-}
-
-
-
-*/
