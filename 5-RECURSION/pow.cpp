97e2ec6709387be97f475a3edccdebad757347f7
Vinay Kumar <vinayp.21.beis@acharya.ac.in>
2025-07-29 10:09:57 +0530

    solved

--- 5-RECURSION/pow.cpp
+++ 5-RECURSION/pow.cpp
@@ -1,23 +1,45 @@
-// RECURSIVE IMPLEMENTATION OF POW( x , n )
+/*
+Implement Pow(x,n) | X raised to the power N
+
+Problem Statement: Given a double x and integer n, calculate x raised to power n. Basically Implement pow(x, n).
+
+Examples:
+
+Example 1:
+
+Input: x = 2.00000, n = 10
+
+Output: 1024.00000
+
+Explanation: You need to calculate 2.00000 raised to 10 which gives ans 1024.00000
+
+Example 2:
+
+Input: x = 2.10000, n = 3
+
+Output: 9.26100
+
+Explanation: You need to calculate 2.10000 raised to 3 which gives ans 9.26100
+
+*/
 
 #include <iostream>
+#include <vector>
+#include <string>
 
-// function to implement the pow
-int powerFunction(int n, int p)
+int pow(int number, int power)
 {
-    if (p == 1)
-        return n;
+    if (power == 1)
+        return number;
 
-    return n * powerFunction(n, p - 1);
+    return number * pow(number, power - 1);
 }
 
 int main()
 {
-    int number = 0, power = 1;
-    std::cin >> number >> power;
+    int number = 2, power = 10;
 
-    int result = powerFunction(number, power);
-    std::cout << result << std::endl;
+    std::cout << pow(number, power) << std::endl;
 
     return 0;
 }
