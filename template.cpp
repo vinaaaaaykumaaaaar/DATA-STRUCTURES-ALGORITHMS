d299fbe359a9992492dc91eaa2dd39bb1098219a
Vinay Kumar <vinayp.21.beis@acharya.ac.in>
2025-08-28 14:32:33 +0530

    solved

--- template.cpp
+++ template.cpp
@@ -1,52 +1,57 @@
-#include <bits/stdc++.h>
+// A simple and clean C++ template for solving DSA problems in coding interviews.
 
-using namespace std;
+#include <iostream>
+#include <set>
+#include <map>
+#include <list>
+#include <vector>
+#include <array>
+#include <deque>
+#include <forward_list>
+#include <stack>
+#include <queue>
+#include <bitset>
+#include <algorithm>
+#include <numeric>
+#include <functional>
+#include <iterator>
+#include <string>
+#include <sstream>
+#include <cmath>
+#include <limits>
+#include <iomanip>
+#include <tuple>
+#include <utility>
+#include <cassert>
+#include <random>
+#include <chrono>
+#include <climits>
+#include <cctype>
+#include <cstring>
+#include <type_traits>
+#include <unordered_set>
+#include <unordered_map>
 
-void brute_force(vector<int> &a)
+// limits use
+int minElement = std::numeric_limits<int>::min();
+int maxElement = std::numeric_limits<int>::max();
+
+class Solution
 {
-}
-
-void better(vector<int> &a)
-{
-}
-
-void optimal(vector<int> &a)
-{
-}
+public:
+};
 
 int main()
 {
+    std::ios_base::sync_with_stdio(false); // Faster input/output
+    std::cin.tie(NULL);
 
-    vector<int> a = {};
-    vector<int> b = {};
-    vector<int> c = {};
-
-    vector<vector<int>> test_cases = {a, b, c};
-
-    cout << "-----------------------------------------------------------------" << endl;
-
-    // Brute force approach test case
-    cout << "--------------------- B R U T E - F O R C E ---------------------" << endl;
-    for (auto test_case : test_cases)
+    int t;
+    std::cin >> t;
+    while (t--)
     {
-        brute_force(test_case);
+        // Solve here
     }
-
-    // Better approach test case
-    cout << "-------------------------- B E T T E R --------------------------" << endl;
-    for (auto test_case : test_cases)
-    {
-        better(test_case);
-    }
-
-    // Optimal approach test case
-    cout << "------------------------  O P T I M A L  ------------------------" << endl;
-    for (auto test_case : test_cases)
-    {
-        optimal(test_case);
-    }
-
-    cout << "-----------------------------------------------------------------" << endl;
 
     return 0;
 }
