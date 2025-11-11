c67e26812cd8523f74bb8fd45f0f45ef17de7d2f
Vinay Kumar <vinayp.21.beis@acharya.ac.in>
2025-11-12 01:56:33 +0530

    solved

---
+++ 0-LOGIC-BUILDING/BASIC-PROBLEMS/closest_number.cpp
@@ -0,0 +1,24 @@
+/*
+Find Closest to n and Divisible by m
+Last Updated : 09 Jul, 2025
+Given two integers n and m (m != 0). Find the number closest to n and divisible by m. If there is more than one such number, then output the one having maximum absolute value.
+
+Examples:
+
+Input: n = 13, m = 4
+Output: 12
+Explanation: 12 is the closest to 13, divisible by 4.
+
+Input: n = -15, m = 6
+Output: -18
+Explanation: Both -12 and -18 are closest to -15, but -18 has the maximum absolute value.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Bye we will see tomorrow" << endl;
+    return 0;
+}
