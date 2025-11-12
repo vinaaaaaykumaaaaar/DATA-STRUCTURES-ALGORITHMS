7d8b647174564874679704c195badd84e554d3aa
Vinay Kumar <vinayp.21.beis@acharya.ac.in>
2025-11-12 13:23:44 +0530

    solved

--- 0-LOGIC-BUILDING/BASIC-PROBLEMS/closest_number.cpp
+++ 0-LOGIC-BUILDING/BASIC-PROBLEMS/closest_number.cpp
@@ -15,10 +15,37 @@ Explanation: Both -12 and -18 are closest to -15, but -18 has the maximum absolu
 */
 
 #include <iostream>
+#include <climits>
 using namespace std;
+
+int closestNumber(int n, int m)
+{
+
+    int closest = 0;
+    int minDifference = INT_MAX;
+
+    // Check numbers around n
+    for (int i = n - abs(m); i <= n + abs(m); ++i)
+    {
+        if (i % m == 0)
+        {
+            int difference = abs(n - i);
+
+            if (difference < minDifference ||
+                (difference == minDifference && abs(i) > abs(closest)))
+            {
+                closest = i;
+                minDifference = difference;
+            }
+        }
+    }
+    return closest;
+}
 
 int main()
 {
-    cout << "Bye we will see tomorrow" << endl;
+    int n = -15, m = 6;
+    cout << closestNumber(n, m) << endl;
+
     return 0;
 }
