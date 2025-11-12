615a898f4083ebb6f9d48d1be04aafc2bcdaf6c5
Vinay Kumar <vinayp.21.beis@acharya.ac.in>
2025-11-12 17:14:38 +0530

    solved

---
+++ 0-LOGIC-BUILDING/EASY/prime_testing.cpp
@@ -0,0 +1,15 @@
+/*
+Introduction to Primality Test and School Method
+Last Updated : 13 Feb, 2025
+Given a positive integer, check if the number is prime or not. A prime is a natural number greater than 1 that has no positive divisors other than 1 and itself. Examples of the first few prime numbers are {2, 3, 5, ...}
+Examples :
+
+Input:  n = 11
+Output: true
+
+Input:  n = 15
+Output: false
+
+Input:  n = 1
+Output: false
+*/
