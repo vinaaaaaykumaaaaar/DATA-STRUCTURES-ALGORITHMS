abe124e258305ff59087213e8d7d2dda71e13be4
Vinay Kumar <vinayp.21.beis@acharya.ac.in>
2026-01-19 12:55:30 +0530

    solved

---
+++ patterns/sixth.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n = 5;
+    
+    for (int i = 5; i > 0; i--)
+    {
+        /* code */
+      for (int j = 0; j < i; j++)
+      {
+        /* code */
+        cout<<j+1;
+      }
+      
+        cout<<endl;
+    
+    }
+    
+}
