a24b158ec005807b1ac17b0c138b58aad936f225
Vinay Kumar <vinayp.21.beis@acharya.ac.in>
2026-01-19 12:48:04 +0530

    solved

---
+++ patterns/forth.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n = 5;
+    
+    for (int i = 0; i < 5; i++)
+    {
+        /* code */
+        for (int j = 0; j < i+1; j++)
+        {
+            /* code */
+            cout<<i+1;
+        }
+        cout<<endl;
+        
+    }
+    
+}
