d1dce93fb351c5bb194ecd637c57429f42eac463
Vinay Kumar <vinayp.21.beis@acharya.ac.in>
2026-03-04 03:36:16 +0530

    solved

--- patterns/ninth.cc
+++ patterns/ninth.cc
@@ -1,9 +1,49 @@
+/*
+
+QUESTION : Pattern 9 TUF
+
+SAMPLE INPUT :
+EXPECTED ANSWER :
+
+    *
+   ***
+  *****
+ *******
+*********
+ *******
+  *****
+   ***
+    *
+
+*/
+
 #include <iostream>
 using namespace std;
+
+void star_pattern()
+{
+    int n = 5;
+    for (int i = 1; i <= 2 * n - 1; i++)
+    {
+        for (int j = 1; j <= abs(n - i); j++)
+        {
+            cout << " ";
+        }
+        for (int j = 1; j <= 2 * (n - abs(i - n)) - 1; j++)
+        {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+}
 
 int main()
 {
     int n = 5;
+
+    cout << "----------------------------------------" << endl;
+    cout << "first approach : " << endl;
 
     // Upper half (including middle)
     for (int i = 1; i <= n; i++)
@@ -24,6 +64,12 @@ int main()
             cout << "*";
         cout << endl;
     }
+
+    cout << "----------------------------------------" << endl;
+
+    cout << "second approach : " << endl;
+    star_pattern();
+    cout << "----------------------------------------" << endl;
 
     return 0;
 }
