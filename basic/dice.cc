815b0277ba01ba7d6684429fe31596f2988b4b6c
Vinay Kumar <vinayp.21.beis@acharya.ac.in>
2026-03-16 14:17:09 +0530

    solved

---
+++ basic/dice.cc
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// debug macro
+
+void _debug_print() { cerr << endl; }
+
+template <typename T, typename... Args>
+void _debug_print(T t, Args... args)
+{
+    cerr << t << " ";
+    _debug_print(args...);
+}
+
+#define debug(...)                             \
+    do                                         \
+    {                                          \
+        cerr << "[" << #__VA_ARGS__ << "] = "; \
+        _debug_print(__VA_ARGS__);             \
+        cerr << endl;                          \
+    } while (0)
+
+// solution class
+
+class Solution
+{
+public:
+    int solFunction(int n)
+    {
+
+        return 7 - n;
+    }
+};
+
+// main function
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    Solution sol;
+
+    int T = 1;
+    // cin >> T;
+    int input;
+
+    while (T--)
+    {
+        cin >> input;
+        cout << sol.solFunction(input) << endl;
+    }
+}
