47e5b3d2a8baf790a08e7b7d3ede31da51977738
Vinay Kumar <vinayp.21.beis@acharya.ac.in>
2026-05-13 02:29:09 +0530

    solved

--- linked-list/basic/implementation.cpp
+++ linked-list/basic/implementation.cpp
@@ -1,22 +1,46 @@
 #include<iostream>
+using namespace std; 
 
-class LinkedList
+class Node
 {
     public : 
         int data; 
-        Node* next; 
+        Node* next;
 
-        // Constructor with data and next
-        Node(int data1, Node* next1)
+        // construtor with no data 
+        Node()
         {
-            data = data1;
-            next = next1;
+            this->data = 0;
+            this->next = nullptr;
         }
 
-        // Constructor with only data
-        Node(int data1) 
+        // construtor with data
+        Node(int data1)
         {
-            data = data1;
-            next = nullptr;
+            this->data = data1;
+            this->next = nullptr;
+        }
+
+        // construtor with data and next pointer
+        Node (int data1 , Node* next1)
+        {
+            this->data = data1;
+            this->next = next1;
         }
 };
+
+int main()
+{
+  
+    Node* C = new Node(3);
+    Node* B = new Node(2,C);
+    Node* A = new Node(1,B);
+
+
+
+    cout<<A->data<<" "<<A->next<<endl;
+    cout<<B->data<<" "<<B->next<<endl;
+    cout<<C->data<<" "<<C->next<<endl;
+
+    return 0;
+}
