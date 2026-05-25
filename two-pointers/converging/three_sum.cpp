2d1c4994e0ee3384e58b64bcadf8c3ac8d9c7009
Vinay Kumar <vinayp.21.beis@acharya.ac.in>
2026-05-26 02:51:11 +0530

    solved: two-pointers/converging/three_sum.cpp

--- two-pointers/converging/three_sum.cpp
+++ two-pointers/converging/three_sum.cpp
@@ -42,6 +42,54 @@ public:
 
         return uni;
     }
+
+    vector<vector<int>> threeSumOptimized(vector<int> nums)
+    {
+        vector<vector<int>> res;
+
+        sort(nums.begin(), nums.end());
+
+        const int n = nums.size();
+
+        for (int i = 0; i < n - 2; i++)
+        {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            int left = i + 1, right = n - 1;
+
+            while (left < right)
+            {
+                long long total = (long long)nums[i] + nums[left] + nums[right];
+
+                if (total > 0)
+                {
+                    --right;
+                }
+                else if (total < 0)
+                {
+                    ++left;
+                }
+                else
+                {
+                    res.push_back({nums[i], nums[left], nums[right]});
+
+                    // Skip all duplicate values to avoid duplicate triplets
+                    while (left < right && nums[left] == nums[left + 1])
+                    {
+                        ++left;
+                    }
+                    while (left < right && nums[right] == nums[right - 1])
+                    {
+                        --right;
+                    }
+                    // Move both pointers to continue searching
+                    ++left;
+                    --right;
+                }
+            }
+        }
+    }
 };
 
 int main()
@@ -49,7 +97,7 @@ int main()
     vector<int> nums = {-1, 0, 1, 2, -1, -1};
 
     Solution sol;
-    vector<vector<int>> res = sol.threeSum(nums);
+    vector<vector<int>> res = sol.threeSumOptimized(nums);
 
     for (auto r : res)
     {
