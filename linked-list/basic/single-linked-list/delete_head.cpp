2fca67bedddff4f3bd3abb10eb9178b194653f7d
Vinay Kumar <vinayp.21.beis@acharya.ac.in>
2026-05-26 15:04:19 +0530

    solved: linked-list/basic/single-linked-list/

---
+++ linked-list/basic/single-linked-list/delete_head.cpp
@@ -0,0 +1,88 @@
+#include<iostream>
+using namespace std; 
+
+class Node
+{
+    public : 
+        int data; 
+        Node* next;
+
+        // construtor with no data 
+        Node()
+        {
+            this->data = 0;
+            this->next = nullptr;
+        }
+
+        // construtor with data
+        Node(int data1)
+        {
+            this->data = data1;
+            this->next = nullptr;
+        }
+
+        // construtor with data and next pointer
+        Node (int data1 , Node* next1)
+        {
+            this->data = data1;
+            this->next = next1;
+        }
+};
+
+
+
+
+class LinkedList_Operation
+{
+    public : 
+        // Function to insert at the head position
+        Node* insert_at_head(Node* head , int data1)
+        {
+            Node* new_node = new Node(data1 , head);
+            return new_node;
+        }
+
+        // Function to delete at the head position
+        Node* delete_at_head(Node* head)
+        {
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+            return head;
+        }
+
+        // Print all the nodes
+        void print_list(Node* head) {
+            Node* temp = head;
+            while (temp != nullptr) {
+                cout << temp->data << " ";
+                temp = temp->next;
+            }
+            cout << endl;
+        }
+};
+
+int main()
+{
+  
+    Node* head = new Node(2);
+    head->next = new Node(3);
+
+    LinkedList_Operation link;
+
+    cout<<"Original List : ";
+    link.print_list(head);
+
+    head = link.insert_at_head(head , 1);
+
+    cout<<"Inserted at head operation : ";
+    link.print_list(head);
+
+    head = link.delete_at_head(head);
+
+    cout<<"Delete at head operation : ";
+    link.print_list(head);
+
+
+    return 0;
+}
