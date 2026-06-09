ae132f1f18da6d7f3958c2caa105b86c4ffd6e87
Vinay Kumar <vinayp.21.beis@acharya.ac.in>
2026-06-10 01:07:18 +0530

    solved: patterns/eight.cpp

---
+++ patterns/eight.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 1; j <= 10 - (2 * i - 1); j++)
+        {
+            cout << "*";
+        }
+
+        cout << "\n";
+    }
+}
