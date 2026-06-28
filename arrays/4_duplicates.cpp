596bc1215e3dae08ca5a8b2b0b3a54a67a633882
Vinay Kumar <vinayp.21.beis@acharya.ac.in>
2026-06-28 19:28:16 +0530

    solved: arrays/4_duplicates.cpp

---
+++ arrays/4_duplicates.cpp
@@ -0,0 +1,44 @@
+/*
+Remove Duplicates in-place from Sorted Array
+
+Problem Statement: Given an integer array sorted in non-decreasing order, remove the duplicates in place such that each unique element appears only once. The relative order of the elements should be kept the same.
+
+If there are k elements after removing the duplicates, then the first k elements of the array should hold the final result. It does not matter what you leave beyond the first k elements.
+
+Input: arr[]=[1,1,2,2,2,3,3]
+Output: [1,2,3,_,_,_,_]
+Explanation: Total number of unique elements are 3, i.e[1,2,3] and Therefore return 3 after assigning [1,2,3] in the beginning of the array.
+Input: arr[]=[1,1,1,2,2,3,3,3,3,4,4]
+Output: [1,2,3,4,_,_,_,_,_,_,_]
+Explanation: Total number of unique elements are 4, i.e[1,2,3,4] and Therefore return 4 after assigning [1,2,3,4] in the beginning of the array.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int removeDuplicates(vector<int> &v)
+{
+    if (v.empty())
+        return 0;
+
+    int i = 0;
+    for (int j = 1; j < v.size(); j++)
+        if (v[i] != v[j])
+            v[++i] = v[j];
+
+    return i + 1;
+}
+
+int main()
+{
+    vector<int> v = {1, 1, 1, 2, 2, 3, 3, 3, 3, 4, 4};
+
+    int size = removeDuplicates(v);
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << v[i] << " ";
+    }
+
+    cout << endl;
+}
