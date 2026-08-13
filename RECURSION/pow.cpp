3fbec8440ec2cde534e2dd2c64ae2ac003d872a0
Vinay Kumar <vinayp.21.beis@acharya.ac.in>
2026-08-14 02:33:10 +0530

    solved: RECURSION/pow.cpp

--- RECURSION/pow.cpp
+++ RECURSION/pow.cpp
@@ -33,11 +33,45 @@ double myPowBruteForce(double x, int n)
     return result;
 }
 
+double powOptimal(double x, int n)
+{
+    long long N = n;
+
+    double result = 1;
+
+    if (N == 0)
+        return 1;
+    if (N < 0)
+    {
+        x = 1 / x;
+        N = -N;
+    }
+
+    int count = 0;
+
+    while (N > 0)
+    {
+        cout << "step " << count << ": " << "N = " << N << " x = " << x << " result = " << result << endl;
+        if (N % 2 == 1)
+        {
+            result *= x;
+        }
+
+        x *= x;
+        N /= 2;
+        count++;
+    }
+
+    return result;
+}
+
 int main()
 {
-    cout << myPowBruteForce(2.0, 10) << endl; // 1024
-    cout << myPowBruteForce(2.0, -2) << endl; // 0.25
-    cout << myPowBruteForce(2.1, 3) << endl;  // 9.261
+    // cout << myPowBruteForce(2.0, 10) << endl; // 1024
+    // cout << myPowBruteForce(2.0, -2) << endl; // 0.25
+    // cout << myPowBruteForce(2.1, 3) << endl;  // 9.261
+
+    cout << powOptimal(2, 6) << endl;
 
     return 0;
 }
