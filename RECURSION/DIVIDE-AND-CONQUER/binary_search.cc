8915e161d9c4e2692f3ec918de7aa686e0d21fd8
Vinay Kumar <vinayp.21.beis@acharya.ac.in>
2026-08-17 23:33:43 +0530

    solved: RECURSION/DIVIDE-AND-CONQUER/binary_search.cc

--- RECURSION/DIVIDE-AND-CONQUER/binary_search.cc
+++ RECURSION/DIVIDE-AND-CONQUER/binary_search.cc
@@ -3,26 +3,26 @@
 
 using namespace std;
 
-int binary_search(vector<int> &a, int t, int l, int h)
+int binary_search(vector<int> &arr, int target, int start, int end)
 {
-    int m = l + (h - l) / 2;
-    if (l < h)
-    {
-        if (a[m] == t)
-            return m;
-        else if (a[m] > t)
-            binary_search(a, t, l, m - 1);
-        else
-            binary_search(a, t, m + 1, h);
-    }
-    return -1;
+
+    if (start > end)
+        return -1;
+    int mid = start + (end - start) / 2;
+
+    if (arr[mid] == target)
+        return mid;
+    else if (arr[mid] > target)
+        return binary_search(arr, target, start, mid - 1);
+    else
+        return binary_search(arr, target, mid + 1, end);
 }
 
 int main()
 {
 
     vector<int> arr = {1, 2, 3, 4, 5, 6};
-    int target = 10;
+    int target = 4;
     cout << binary_search(arr, target, 0, arr.size() - 1) << endl;
 
     return 0;
