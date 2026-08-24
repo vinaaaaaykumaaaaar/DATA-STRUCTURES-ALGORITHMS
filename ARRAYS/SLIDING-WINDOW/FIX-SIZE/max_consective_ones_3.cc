5875730904aa60376c8ea5f98301e84d68fb213c
Vinay Kumar <vinayp.21.beis@acharya.ac.in>
2026-08-25 02:49:22 +0530

    solved: ARRAYS/SLIDING-WINDOW/FIX-SIZE/max_consective_ones_3.cc

--- ARRAYS/SLIDING-WINDOW/FIX-SIZE/max_consective_ones_3.cc
+++ ARRAYS/SLIDING-WINDOW/FIX-SIZE/max_consective_ones_3.cc
@@ -3,6 +3,31 @@
 
 using namespace std;
 
+int sliding_window(vector<int> &arr, int k)
+{
+    int n = arr.size();
+    int left = 0;
+    int max_count = 0;
+    int kz = 0; // zero count
+
+    for (int right = 0; right < n; right++)
+    {
+        if (arr[right] == 0)
+            kz++;
+
+        while (kz > k)
+        {
+            if (arr[left] == 0)
+                kz--;
+            left++;
+        }
+
+        max_count = max(max_count, right - left + 1);
+    }
+
+    return max_count;
+}
+
 int main()
 {
     vector<int> arr = {1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0};
@@ -27,6 +52,9 @@ int main()
     }
 
     cout << max_count << endl;
+    cout << "\n\n";
+
+    cout << sliding_window(arr, k) << endl;
 
     return 0;
 }
