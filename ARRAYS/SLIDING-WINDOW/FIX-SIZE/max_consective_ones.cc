6c978904a2f6a159c931b5c76e095c21d840b298
Vinay Kumar <vinayp.21.beis@acharya.ac.in>
2026-08-25 01:10:09 +0530

    solved: ARRAYS/SLIDING-WINDOW/FIX-SIZE/max_consective_ones.cc

--- ARRAYS/SLIDING-WINDOW/FIX-SIZE/max_consective_ones.cc
+++ ARRAYS/SLIDING-WINDOW/FIX-SIZE/max_consective_ones.cc
@@ -2,28 +2,61 @@
 #include <vector>
 using namespace std;
 
-int main()
+int max_consective(vector<int> &arr)
 {
-    vector<int> arr = {1, 1, 0, 1, 1, 1};
+    int n = arr.size();
+    int max_count = 0;
 
-    int max_ones = 0;
-
-    for (int i = 0; i < arr.size(); i++)
+    int i = 0;
+    while (i < n)
     {
-        int count = 0;
-
-        for (int j = i; j < arr.size(); j++)
+        if (arr[i] == 0)
         {
-            if (arr[j] == 0)
-                break;
-
-            count++;
+            i++;
+            continue;
         }
 
-        max_ones = max(max_ones, count);
+        int j = i;
+        int count = 0;
+        while (j < n && arr[j] == 1)
+        {
+            count++;
+            j++;
+        }
+
+        max_count = max(max_count, count);
+        i = j;
     }
 
-    cout << max_ones << endl;
+    return max_count;
+}
+
+int sliding_window(vector<int> arr)
+{
+    int n = arr.size();
+    int max_ones = 0;
+    int l = 0;
+
+    for (int r = 0; r < n; r++)
+    {
+        if (arr[r] == 0)
+        {
+            l = r + 1;
+        }
+        else
+        {
+            max_ones = max(max_ones, r - l + 1);
+        }
+    }
+
+    return max_ones;
+}
+
+int main()
+{
+    vector<int> arr = {1, 1, 1, 1, 0, 1, 1, 1};
+
+    cout << sliding_window(arr) << endl;
 
     return 0;
 }
