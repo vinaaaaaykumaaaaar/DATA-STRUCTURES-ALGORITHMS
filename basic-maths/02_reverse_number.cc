21e99376449d1bb81b8fc38fb8369057393a8828
Vinay Kumar <vinayp.21.beis@acharya.ac.in>
2026-09-03 01:23:35 +0530

    reverse numbers

---
+++ basic-maths/02_reverse_number.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int rev = 0, rem;
+
+    while (n > 0)
+    {
+        rem = n % 10;
+        rev = rev * 10 + rem;
+        n = n / 10;
+    }
+
+    printf("%d", rev);
+}
--- temp_black_format.py
+++ temp_black_format.py
@@ -1,26 +1,14 @@
-n = input("enter the number")
+n = input("enter the number to reverse : ")
+
+i = 0 
+j = len(n) - 1
+
+while(i<j):
+    temp = n[i]
+    n[i] = n[j]
+    n[j] = temp
+    i+=1 
+    j-=1
 
 
-count = 0
-
-for i in range(0,len(n)):
-    count+=1
-
-print(count)
-
-
-
-n1 = int(n)
-
-c1 = 0
-
-while(n1>0):
-    n1 = n1 // 10
-    print(n1)
-    c1+=1
-
-print(c1)
-
-
-    
-    
+print(n)
