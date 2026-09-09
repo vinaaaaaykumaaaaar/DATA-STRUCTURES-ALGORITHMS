db5a761daf8097b35a7031a31430f89e91471833
Vinay Kumar <vinayp.21.beis@acharya.ac.in>
2026-09-09 14:43:22 +0530

    updated

---
+++ stacks/implementation-basics/linked_list_implementation.cc
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <stdexcept>
+
+using namespace std;
+
+class Stack
+{
+private:
+    struct Node
+    {
+        int value;
+        Node *next;
+
+        Node(int value, Node *next = nullptr)
+        {
+            this->value = value;
+            this->next = next;
+        }
+    };
+
+    Node *topNode;
+    int stackSize;
+
+public:
+    Stack()
+    {
+        topNode = nullptr;
+        stackSize = 0;
+    }
+
+    ~Stack()
+    {
+        while (!empty())
+        {
+            pop();
+        }
+    }
+
+    void push(int value)
+    {
+        Node *newNode = new Node(value);
+
+        newNode->next = topNode;
+        topNode = newNode;
+        stackSize++;
+    }
+
+    void pop()
+    {
+        if (empty())
+        {
+            throw underflow_error("Stack is empty");
+        }
+
+        Node *temp = topNode;
+
+        topNode = topNode->next;
+
+        delete temp;
+
+        stackSize--;
+    }
+
+    int top() const
+    {
+        if (empty())
+        {
+            throw underflow_error("Stack is empty");
+        }
+
+        return topNode->value;
+    }
+
+    bool empty() const
+    {
+        return topNode == nullptr;
+    }
+
+    int size() const
+    {
+        return stackSize;
+    }
+};
+
+int main()
+{
+
+    Stack st;
+
+    st.push(10);
+    st.push(20);
+    st.push(30);
+
+    cout << st.top() << endl; // 30
+
+    st.pop();
+
+    cout << st.top() << endl; // 20
+
+    return 0;
+}
