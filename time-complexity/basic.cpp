7e1d90733250162f32ee7ee4029fb54578bc38cc
Vinay Kumar <vinayp.21.beis@acharya.ac.in>
2026-09-19 22:15:25 +0530

    updated

--- time-complexity/basic.cpp
+++ time-complexity/basic.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 // fundamental idea is -> 1 operation = 1 simple C++ statement: +, -, *, /, =, if, a[i]
 
 int main()
 {
+    int n;
+    vector<int> arr;
+
+    // linear time complexity
+    int sum = 0; // 1 op
+    for (int i = 0; i < n; i++)
+    {                  // n+1 checks
+        sum += arr[i]; // n ops
+    }
 }
