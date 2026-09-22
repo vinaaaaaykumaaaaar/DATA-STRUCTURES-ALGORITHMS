f175a203c713753670248f98b7590a2ad2bbdb1e
Vinay Kumar <vinayp.21.beis@acharya.ac.in>
2026-09-22 23:01:27 +0530

    updated

---
+++ recursion/linear-recursion/print_1.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <cmath>
+#include <climits>
+#include <numeric>
+#include <utility>
+#include <functional>
+#include <bitset>
+#include <cstring>
+using namespace std;
+#define ll long long
+
+#define debug(x) cerr << #x << " = " << (x) << "\n"
+#define debugArr(a, n)                               \
+    cerr << #a << " = [";                            \
+    for (int i = 0; i < (n); i++)                    \
+        cerr << (a)[i] << (i + 1 < (n) ? ", " : ""); \
+    cerr << "]\n"
+#define debugVec(v)        \
+    cerr << #v << " = [";  \
+    for (auto &i : v)      \
+        cerr << i << ", "; \
+    cerr << "]\n"
+
+void print_n(int n)
+{
+    if (n <= 0)
+        return;
+
+    print_n(n - 1);
+    cout << n << endl;
+}
+
+void solve()
+{
+    print_n(10);
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    solve();
+
+    return 0;
+}
