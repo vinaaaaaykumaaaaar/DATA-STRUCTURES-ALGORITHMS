e7ba206cf0826c972773cc9a201fdc86e9df83e2
Vinay Kumar <vinayp.21.beis@acharya.ac.in>
2026-09-24 09:32:13 +0530

    updated

---
+++ binary-trees/implementation/implementation.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode(int x)
+    {
+        val = x;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+int main()
+{
+
+    // creating the tree
+
+    TreeNode *root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+
+    return 0;
+}
